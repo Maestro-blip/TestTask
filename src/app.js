3655cdc8aacfa48f3c442fcdaf919f798bc9b39a
Maestro <volodimirkucak@icloud.com>
2025-12-23 16:35:58 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,34 @@
 const express = require('express');
 require('dotenv').config();
-
 const router = require('./routes/api')
-
+const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 const app = express();
 
 const PORT = process.env.PORT ?? 8000;
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, 
+	limit: 100, 
+	standardHeaders: 'draft-8',
+	legacyHeaders: false
+})
+
+
+app.use(morgan('combined'));
+
 app.use(express.json());
 
+app.use(limiter)
 app.use('/api',router);
 
-app.use((err,req,res,next) => {
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
 
-    console.error(err)
-    res.status(500).send("Global error")
-    next()
+app.use((err,req,res,next) => {
+console.error(err);
+  res.status(500).json({ message: err.message});
 })
 
 app.listen(PORT,()=>console.log(`Server listening on http://localhost:${PORT}`))
