da3df5862f524d44a5e5e11285a169de6c4cbca3
Maestro <volodimirkucak@icloud.com>
2025-12-23 16:31:24 +0200

    Add validation middleware and JSON Schem

---
+++ src/middleware/appoint.js
@@ -0,0 +1,19 @@
+const Ajv = require("ajv");
+const express = require('express');
+const ajv = new Ajv();
+const appointSchema = require('../schema/appoint.json')
+
+
+function validDate(req,res,next){
+const validate = ajv.compile(appointSchema)
+
+const valid = validate(req.body)
+    if(!valid){
+        console.log(validate.errors)
+        return res.status(400).send('bed data')
+}
+
+next()
+}
+
+module.exports = { validDate }
