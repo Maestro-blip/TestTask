2c6ff9d31a4b29aa8e0b2120b74c6cecc5fe5923
Maestro <volodimirkucak@icloud.com>
2025-12-23 14:46:04 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,4 +11,11 @@ app.use(express.json());
 
 app.use('/api',router);
 
+app.use((err,req,res,next) => {
+
+    console.error(err)
+    res.status(500).send("Global error")
+    next()
+})
+
 app.listen(PORT,()=>console.log(`Server listening on http://localhost:${PORT}`))
